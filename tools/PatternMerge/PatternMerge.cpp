e4edf2346dbb438e3baed95b479eee80df2cebfb
Juhang Park <juhang3@daum.net>
2023-03-27 14:00:51 +0900

    tool add-on
    Pattern Merge, waypoint generator

---
+++ tools/PatternMerge/PatternMerge.cpp
@@ -0,0 +1,197 @@
+﻿#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+// Define constants for the tile size and number of tiles
+const int TILE_SIZE = 16;
+int NUM_TILES = 2;
+int NUM_TILES_X = 2;
+int NUM_TILES_Y = 1;
+int NUM_MERGE_COUNT = 1;
+
+typedef struct tag_Color
+{
+    uint8_t R;
+    uint8_t G;
+    uint8_t B;
+}Color;
+
+Uint32 SDL_GetPixel(SDL_Surface* surface, int x, int y);
+bool SetPixel(SDL_Surface* surface, int x, int y, uint8_t r, uint8_t g, uint8_t b);
+
+SDL_Surface* mergeTiles(SDL_Surface* tiles[], int mergeTileStart, int mergeTileEnd, Uint32 backgroundColor, Color secondColor) {
+    // Create the output surface
+    int tileSizeX = (mergeTileEnd - mergeTileStart + 1)* TILE_SIZE / 2;
+    SDL_Surface* outputSurface = SDL_CreateRGBSurface(0, tileSizeX, TILE_SIZE, 24, 0, 0, 0, 0);
+
+    int mergeloop = (mergeTileEnd - mergeTileStart + 1) / 2;
+
+    for (int i = 0; i < mergeloop; i++)
+    {
+        int tileIndex1 = (i * 2 + mergeTileStart) ;
+        int tileIndex2 = (i * 2 + mergeTileStart) + 1;
+        // Copy the pixels from the tiles to the output surface
+        for (int y = 0; y < TILE_SIZE; y++) {
+            for (int x = 0; x < TILE_SIZE; x++) {
+
+                Uint32 tile1Pixel = SDL_GetPixel(tiles[tileIndex1], x, y);
+
+                if (x < TILE_SIZE && y < TILE_SIZE && tile1Pixel != backgroundColor) {
+                    Color current;
+                    current.R = tile1Pixel;
+                    current.G = tile1Pixel >> 8;
+                    current.B = tile1Pixel >> 16;
+                    SetPixel(outputSurface, x + TILE_SIZE * i, y, current.R, current.G, current.B);
+                }
+
+                Uint32 tile2Pixel = SDL_GetPixel(tiles[tileIndex2], x, y);
+
+                if (x < TILE_SIZE && y < TILE_SIZE && tile2Pixel != backgroundColor) {
+                    /*Color current;
+                    current.R = tile2Pixel;
+                    current.G = tile2Pixel >> 8;
+                    current.B = tile2Pixel >> 16;*/
+                    SetPixel(outputSurface, x + TILE_SIZE * i, y, secondColor.R, secondColor.G, secondColor.B);
+
+                }
+            }
+        }
+    }
+
+    return outputSurface;
+}
+
+Uint32 SDL_GetPixel(SDL_Surface * surface, int x, int y) {
+    int bpp = surface->format->BytesPerPixel;
+    Uint8* p = (Uint8*)surface->pixels + y * surface->pitch + x * bpp;
+    switch (bpp) {
+    case 1:
+        return *p;
+    case 2:
+        return *(Uint16*)p;
+    case 3:
+        if (SDL_BYTEORDER == SDL_BIG_ENDIAN) {
+            return p[0] << 16 | p[1] << 8 | p[2];
+        }
+        else {
+            return p[0] | p[1] << 8 | p[2] << 16;
+        }
+    case 4:
+        return *(Uint32*)p;
+    default:
+        return 0;
+    }
+}
+
+
+#undef main
+int main(int argc, char* argv[]) {
+    // Initialize SDL and SDL_image
+    SDL_Init(SDL_INIT_VIDEO);
+    IMG_Init(IMG_INIT_PNG);
+
+    // Check that a command-line argument was provided
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <input_file.png>" << std::endl;
+        return 1;
+    }
+
+    // Load the input PNG file
+    const char* inputFile = argv[1];
+    SDL_Surface* inputSurface = IMG_Load(inputFile);
+    if (!inputSurface) {
+        std::cerr << "Failed to load input file: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    // Check that the input file size is a multiple of 16
+    if ((inputSurface->w % 16) != 0 || (inputSurface->h % 16) != 0) {
+        std::cerr << "Input file size must be a multiple of 16" << std::endl;
+        return 1;
+    }
+
+    NUM_TILES_X = inputSurface->w / 16;
+    NUM_TILES_Y = inputSurface->h / 16;
+    NUM_TILES = NUM_TILES_X * NUM_TILES_Y;
+    NUM_MERGE_COUNT = NUM_TILES / 2;
+    
+    // Create an array to hold the individual tiles
+    // Allocate the array
+    SDL_Surface** tiles = (new SDL_Surface * [NUM_TILES]);
+
+    
+    // Extract the tiles from the input image
+    for (int i = 0; i < NUM_TILES_X; i++) {
+        for (int j = 0; j < NUM_TILES_Y; j++) {
+            int tileX = i * TILE_SIZE;
+            int tileY = j * TILE_SIZE;
+            tiles[i + j * NUM_TILES_X] = (SDL_Surface*)SDL_CreateRGBSurface(0, TILE_SIZE, TILE_SIZE, 32, 0, 0, 0, 0);
+            SDL_Rect srcRect = { tileX, tileY, TILE_SIZE, TILE_SIZE };
+            SDL_Rect dstRect = { 0, 0, TILE_SIZE, TILE_SIZE };
+            SDL_BlitSurface(inputSurface, &srcRect, tiles[i + j * NUM_TILES_X], &dstRect);
+        }
+    }
+    /*Uint32 background = 16711935;
+    Color current;
+    current.R = background;
+    current.G = background >> 8;
+    current.B = background >> 16;
+    */
+    //Uint32 background = SDL_GetPixel(inputSurface, 8, 0);
+
+    int mergeTileStart = 54;
+    int mergeTileEnd = 55;
+    Uint32 background = 0;
+
+    Uint32 secondColor = 0xbb5555;
+    Color color;
+    color.R = secondColor;
+    color.G = secondColor >> 8;
+    color.B = secondColor >> 16;
+    SDL_Surface* outputSurface = mergeTiles(tiles, mergeTileStart, mergeTileEnd, background, color);
+
+    // Write the output image to a new PNG file
+    IMG_SavePNG(outputSurface, "output.png");
+
+  
+    // Clean up resources and exit
+    SDL_FreeSurface(inputSurface);
+    for (int i = 0; i < NUM_TILES; i++) {
+        SDL_FreeSurface(tiles[i]);
+    }
+
+    // Deallocate the array
+    delete[] tiles;
+
+    SDL_FreeSurface(outputSurface);
+
+    IMG_Quit();
+    SDL_Quit();
+
+    return 0;
+}
+
+bool SetPixel(SDL_Surface* surface, int x, int y, uint8_t r, uint8_t g, uint8_t b)
+{
+    int bpp = surface->format->BytesPerPixel;
+
+    SDL_assert(bpp == 3);
+
+    Uint8* p = (Uint8*)surface->pixels + y * surface->pitch + x * bpp;
+
+
+    if (SDL_BYTEORDER == SDL_BIG_ENDIAN)
+    {
+        p[0] = b;
+        p[1] = g;
+        p[2] = r;
+    }
+    else
+    {
+        p[0] = r;
+        p[1] = g;
+        p[2] = b;
+    }
+
+    return true;
+}
---
+++ tools/waypoints_generator/waypoints_generator.cpp
@@ -0,0 +1,196 @@
+﻿#include <SDL2/SDL.h>
+#include <stdio.h>
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+#define WORLD_WIDTH 256
+#define WORLD_HEIGHT 192
+
+// 실제 좌표를 화면 좌표로 변환하는 함수
+int to_screen_coord_x(int world_coord) {
+    return world_coord * SCREEN_WIDTH / WORLD_WIDTH;
+}
+
+int to_screen_coord_y(int world_coord) {
+    return world_coord * SCREEN_HEIGHT / WORLD_HEIGHT;
+}
+
+// 화면 좌표를 실제 좌표로 변환하는 함수
+int to_world_coord_x(int screen_coord) {
+    return screen_coord * WORLD_WIDTH / SCREEN_WIDTH;
+}
+
+int to_world_coord_y(int screen_coord) {
+    return screen_coord * WORLD_HEIGHT / SCREEN_HEIGHT;
+}
+
+// Define a structure to hold the waypoint data
+typedef struct {
+    int x;
+    int y;
+} Waypoint;
+
+// Define a function to generate the waypoints
+void generateWaypoints(Waypoint* waypoints, int numWaypoints) {
+    int screenWidth = 640;
+    int screenHeight = 480;
+
+    // Generate the waypoints randomly within the screen bounds
+    for (int i = 0; i < numWaypoints; i++) {
+        waypoints[i].x = rand() % screenWidth;
+        waypoints[i].y = rand() % screenHeight;
+    }
+}
+
+
+void saveWaypointStructToHeaderFile(const char* filename);
+void saveWaypointsToHeaderFile(Waypoint * waypoints, int numWaypoints, const char* structName, const char* varName, const char* filename);
+
+int main(int agrc, char** argv) {
+    // Define the number of waypoints to generate
+    const int MAX_WAYPOINTS = 100;
+
+    // Allocate memory for the waypoints
+    Waypoint* waypoints = (Waypoint*)malloc(sizeof(Waypoint) * MAX_WAYPOINTS);
+    int numWaypoints = 0;
+
+    // Initialize SDL
+    SDL_Init(SDL_INIT_VIDEO);
+
+    // Create a window
+    SDL_Window* window = SDL_CreateWindow("Waypoint Generation Example",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640, 480, SDL_WINDOW_SHOWN);
+
+    // Create a renderer
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+    // Generate the initial waypoints
+    generateWaypoints(waypoints, numWaypoints);
+
+    // Update the window
+    SDL_RenderPresent(renderer);
+
+    // Wait for a key press before exiting
+    SDL_Event e;
+    int quit = 0;
+    // While application is running
+    while (!quit) {
+        // Handle events on queue
+        while (SDL_PollEvent(&e) != 0) {
+            // User requests quit
+            if (e.type == SDL_QUIT) {
+                quit = 1;
+            }
+            // Handle mouse button events
+            if (e.type == SDL_MOUSEBUTTONDOWN) {
+                if (e.button.button == SDL_BUTTON_LEFT) {
+                    // Add a new waypoint where the mouse was clicked
+                    if (numWaypoints < MAX_WAYPOINTS) {
+                        waypoints[numWaypoints].x = e.button.x;
+                        waypoints[numWaypoints].y = e.button.y;
+                        numWaypoints++;
+                    }
+                }
+                else if (e.button.button == SDL_BUTTON_RIGHT) {
+                    // Remove the last waypoint
+                    if (numWaypoints > 0) {
+                        numWaypoints--;
+                    }
+                }
+            }
+        }
+
+        // Clear screen
+        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
+        SDL_RenderClear(renderer);
+
+        // Render the waypoints as circles
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+        for (int i = 0; i < numWaypoints; i++) {
+            SDL_RenderDrawPoint(renderer, waypoints[i].x, waypoints[i].y);
+            SDL_RenderDrawPoint(renderer, waypoints[i].x + 1, waypoints[i].y);
+            SDL_RenderDrawPoint(renderer, waypoints[i].x, waypoints[i].y + 1);
+            SDL_RenderDrawPoint(renderer, waypoints[i].x + 1, waypoints[i].y + 1);
+            SDL_RenderDrawPoint(renderer, waypoints[i].x - 1, waypoints[i].y);
+            SDL_RenderDrawPoint(renderer, waypoints[i].x, waypoints[i].y - 1);
+            SDL_RenderDrawPoint(renderer, waypoints[i].x - 1, waypoints[i].y - 1);
+            SDL_RenderDrawPoint(renderer, waypoints[i].x - 1, waypoints[i].y + 1);
+
+            if (i < numWaypoints - 1) {
+                // Connect the waypoints with lines
+                SDL_RenderDrawLine(renderer, waypoints[i].x, waypoints[i].y, waypoints[i + 1].x, waypoints[i + 1].y);
+            }
+        }
+
+        // Update the window
+        SDL_RenderPresent(renderer);
+    }
+
+    saveWaypointStructToHeaderFile("Waypoint.h");
+    saveWaypointsToHeaderFile(waypoints, numWaypoints, "Waypoint", "pattern1", "Pattern1.h");
+
+    // Destroy the renderer and window
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    // Free the memory
+    free(waypoints);
+
+    // Quit SDL
+    SDL_Quit();
+
+    return 0;
+}
+
+void saveWaypointStructToHeaderFile(const char* filename) {
+    FILE* fp = fopen(filename, "w");
+    if (fp == NULL) {
+        printf("Failed to open file for writing.\n");
+        return;
+    }
+
+    // Waypoint 구조체 선언문 작성
+    fprintf(fp, "#ifndef WAYPOINT_H\n#define WAYPOINT_H\n\n");
+    fprintf(fp, "typedef struct {\n    int x;\n    int y;\n} Waypoint;\n\n");
+    fprintf(fp, "#endif // WAYPOINT_H");
+
+    fclose(fp);
+}
+
+void saveWaypointsToHeaderFile(Waypoint* waypoints, int numWaypoints, const char* structName, const char* varName, const char* filename) {
+    FILE* fp = fopen(filename, "w");
+    if (fp == NULL) {
+        printf("Failed to open file for writing.\n");
+        return;
+    }
+
+    // 헤더 파일에 구조체와 변수 선언문 작성
+    fprintf(fp, "#ifndef %s_H\n#define %s_H\n\n", varName, varName);
+    fprintf(fp, "#include \"%s.h\"\n\n", structName);
+    fprintf(fp, "const int num%s = %d;\n", varName, numWaypoints);
+    fprintf(fp, "%s %s[] = {\n", structName, varName);
+
+    // Waypoint 배열 데이터 저장
+    for (int i = 0; i < numWaypoints; i++) {
+        fprintf(fp, "    {%d, %d}", waypoints[i].x, waypoints[i].y);
+        if (i < numWaypoints - 1) {
+            fprintf(fp, ",");
+        }
+        fprintf(fp, "\n");
+    }
+
+    // 헤더 파일 끝부분 작성
+    fprintf(fp, "};\n\n#endif // %s_H", varName);
+
+    fclose(fp);
+}
+
+
+
+
+
+
+    
