2e2a39b5c3436726aafe08cfd1fc05acebabf046
Juhang Park <juhang3@daum.net>
2023-10-20 20:41:25 +0900

    refactoring

--- game/src/main.c
+++ game/src/main.c
@@ -32,9 +32,8 @@ size_t GetMapDataSize(const uint8_t* map)
 
 void ap_uncompress(const uint8_t* dst, const uint8_t* src)
 {
-
-   // size_t n = GetMapDataSize(src - 3);    
-    apultra_decompress(src, (uint8_t*)dst, 768, 32 * 24, 0, 0);
+    size_t n = GetMapDataSize(src - 3);    
+    apultra_decompress(src, (uint8_t*)dst, n, 32 * 24, 0, 0);
 }
 uint8_t* VIDEO_MEMORY_ADDRESS = 0;
 #else
